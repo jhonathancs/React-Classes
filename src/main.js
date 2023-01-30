d06933c7a4505595aa2251c6d4ba89239a548341
Jhonathan Costa <jhonathan_jcs@hotmail.com>
2023-01-30 16:10:00 -0400

    1.1
    funcao range

--- src/main.js
+++ src/main.js
@@ -3,9 +3,20 @@ import copy from 'clipboard-copy';
 
 const passwordBtnEl = document.querySelector('button');
 const displayPasswordEl = document.querySelector('h2');
-passwordBtnEl.addEventListener('click', () => {
-  const randomPassword = nanoid();
-  displayPasswordEl.innerHTML = randomPassword;
+const rangeInput = document.getElementById('length-range');
+const numberRange = document.getElementById('length')
+
+
+// passwordBtnEl.addEventListener('click', () => {
+//   const randomPassword = nanoid();
+//   displayPasswordEl.innerHTML = randomPassword;
+// });
+
+rangeInput.addEventListener("input", function() {
+  // const randomPass = nanoid()
+  displayPasswordEl.innerHTML = nanoid(this.value);
+  numberRange.value = this.value;
+  // console.log(this.value);
 });
 
 displayPasswordEl.addEventListener('click', (event) => {
