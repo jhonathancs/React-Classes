63e6697878d4a051317ba6dbae89c452b53ad054
Gilson.Fernandes <85591493+DevJunior21@users.noreply.github.com>
2023-02-22 21:35:57 -0300

    Projeto Consumindo API finalizado
    * Create index.html
    
    start
    
    * 3.1
    
    casa-cambio
    
    * 3.1
    
    finals-cambio-casa
    
    * Update main.js
    
    Adicionado funcao if, caso a caixa de input esteja vazia, surgir um alert com a lib sweetalert2
    
    * Update main.js
    
    ---------
    
    Co-authored-by: Jhonathan Costa <jhonathan_jcs@hotmail.com>

---
+++ src/main.js
@@ -0,0 +1,61 @@
+// https://exchangerate.host/#/#docs
+// "https://api.exchangerate.host/latest?base="
+import Swal from "sweetalert2";
+const inputText = document.getElementById('inputText');
+const btnSearch = document.getElementById("search");
+const tableCoin = document.getElementById("tableCoin");
+const lineCell = document.getElementById('lineCell')
+const conteinerDiv = document.getElementById('quadrado')
+
+const fetchCoin = (value) => {
+  const result = fetch(`https://api.exchangerate.host/latest?base=${value}`)
+    .then((res) => res.json())
+    .then((data) => {
+      // const  { rates } = data
+      // console.log({rates})
+      console.log(data)
+
+      return data
+    });
+    return result
+}
+
+ 
+  
+btnSearch.addEventListener('click', async () =>{
+
+ if (inputText.value === ''){
+     {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops ...',
+            text: 'Voce Precisa passar uma Moeda!'})
+
+    }}
+
+else{
+  
+    const inputTextInsert = inputText.value
+    const responseApi = await fetchCoin(inputTextInsert);
+    const rates = await responseApi.rates
+    console.log(rates)
+    conteinerDiv.innerHTML = "";
+    for (const rate in rates) {
+
+        const conteiner = document.createElement('div');
+        conteiner.classList.add('divFilha')
+        const colorFont = document.createElement('span')
+        colorFont.classList.add('ColorFont')
+        conteiner.textContent = `${rate}: ${rates[rate]} `
+        conteinerDiv.appendChild(conteiner)
+
+      }
+    
+
+    
+
+}})
+
+
+
+
