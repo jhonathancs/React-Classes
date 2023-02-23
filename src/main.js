37ff2285f32fd09d2775667bb37ee90cb2f72357
Jhonathan Costa <jhonathan_jcs@hotmail.com>
2023-02-22 20:02:21 -0400

    3.1
    finals-cambio-casa

--- src/main.js
+++ src/main.js
@@ -5,39 +5,38 @@ const inputText = document.getElementById('inputText');
 const btnSearch = document.getElementById("search");
 const tableCoin = document.getElementById("tableCoin");
 const lineCell = document.getElementById('lineCell')
+const conteinerDiv = document.getElementById('quadrado')
 
 const fetchCoin = (value) => {
   const result = fetch(`https://api.exchangerate.host/latest?base=${value}`)
     .then((res) => res.json())
     .then((data) => {
+      // const  { rates } = data
+      // console.log({rates})
+      console.log(data)
+
       return data
     });
     return result
 }
+
 btnSearch.addEventListener('click', async () =>{
     const inputTextInsert = inputText.value
     const responseApi = await fetchCoin(inputTextInsert);
     const rates = await responseApi.rates
     console.log(rates)
-    const header = tableCoin.createTHead();
-    const rowHeader = header.insertRow(0);
-    const cellHeaderName = rowHeader.insertCell(0);
-    cellHeaderName.innerHTML = "<b>Nome da moeda</b>";
-    const cellHeaderValue = rowHeader.insertCell(1);
-    cellHeaderValue.innerHTML = "<b>Taxa de câmbio</b>";
-    const cellHeaderCode = rowHeader.insertCell(2);
+    conteinerDiv.innerHTML = "";
     for (const rate in rates) {
-      const line = tableCoin.insertRow(-1);
-      const name = line.insertCell(0);
-      name.innerText = rate;
-      const value = line.insertCell(1);
-      value.innerText = rates[rate];
-      const code = line.insertCell(2);
-      code.innerText = responseApi.base;
-      const updated = line.insertCell(3);
-      updated.innerText = responseApi.date;
+
+        const conteiner = document.createElement('div');
+        conteiner.classList.add('divFilha')
+        conteiner.textContent = `${rate}: ${rates[rate]} `
+        conteinerDiv.appendChild(conteiner)
+
+      }
     }
-  });
+);
+
     
 
 
